122f9c04be397e97ec9d78df6cff7b612037c0ef
Matthew Phillips <matthew@matthewphillips.info>
2017-02-25 12:36:10 -0600

    Removing some unused files

--- vdom-streaming-serializer_test.js
+++
@@ -1,9 +0,0 @@
-import QUnit from 'steal-qunit';
-import plugin from './vdom-streaming-serializer';
-
-QUnit.module('vdom-streaming-serializer');
-
-QUnit.test('Initialized the plugin', function(){
-  QUnit.equal(typeof plugin, 'function');
-  QUnit.equal(plugin(), 'This is the vdom-streaming-serializer plugin');
-});
