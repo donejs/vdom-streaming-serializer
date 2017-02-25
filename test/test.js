7c9474f094a72f71b7eda926c69ff65c0a4d98b0
agc123 <agc123@yeah.net>
2017-02-25 12:03:05 -0600

    create async test and async promise

--- test/test.js
+++ test/test.js
@@ -6,41 +6,86 @@ var makeDocument = require('can-vdom/make-document/make-document');
 var serialize = require('../vdom-streaming-serializer');
 
 describe('vdom-streaming-serializer', function(){
-  it('works', function(done){
-    //assert.ok(true, 'It worked');
-    var document = makeDocument();
+  	it('wait on aync node', function(done){
+    	//assert.ok(true, 'It worked');
+		var document = makeDocument();
 
-	var h1 = document.createElement('h1');
-	h1.appendChild(document.createTextNode('Hello world'));
-	document.body.appendChild(h1);
+		var h1 = document.createElement('h1');
+		h1.appendChild(document.createTextNode('Hello world'));
+		document.body.appendChild(h1);
 
-	var ul = document.createElement('ul');
-	document.body.appendChild(ul);
+		var ul = document.createElement('ul');
+		document.body.appendChild(ul);
 
-	var li = document.createElement('li');
+		var li = document.createElement('li');
 
-	// Marking this li as async will force the serialize to wait
-	li[ASYNC] = Promise.resolve();
 
-	ul.appendChild(li);
+		// Marking this li as async will force the serialize to wait
+		li[ASYNC] = Promise.resolve().then(function(){
+			var span = document.createElement('span');
+			span.appendChild(document.createTextNode('This is interesting'));
+			li.appendChild(span);
+		})
 
-	var stream = serialize(document.documentElement);
+		ul.appendChild(li);
 
-	stream.setEncoding('utf8');
+		var stream = serialize(document.documentElement);
 
-	var count = 0;
-	stream.on('data', function(html){
-		count++;
-		if (count == 1) {
-			//assert.equal(1,2);
-			assert.equal(html, "<html><body><h1>Hello world</h1><ul>");
-		} else if (count == 2) {
-			assert.equal(html, "<li></li></ul></body></html>");
-			done();
-		}
-		
+		stream.setEncoding('utf8');
+
+		var count = 0;
+		stream.on('data', function(html){
+			count++;
+			if (count == 1) {
+				//assert.equal(1,2);
+				assert.equal(html, "<html><body><h1>Hello world</h1><ul>");
+			} else if (count == 2) {
+				//console.log(html);
+				assert.equal(html, "<li><span>This is interesting</span></li></ul></body></html>");
+				done();
+			}			
+		});
 	});
-  });
+
+	it('works', function(done){
+    //assert.ok(true, 'It worked');
+
+		var document = makeDocument();
+
+		var h1 = document.createElement('h1');
+		h1.appendChild(document.createTextNode('Hello world'));
+		document.body.appendChild(h1);
+
+		var ul = document.createElement('ul');
+		document.body.appendChild(ul);
+
+		var li = document.createElement('li');
+
+
+		// Marking this li as async will force the serialize to wait
+		li[ASYNC] = Promise.resolve();
+
+		ul.appendChild(li);
+
+		var stream = serialize(document.documentElement);
+
+		stream.setEncoding('utf8');
+
+		var count = 0;
+		stream.on('data', function(html){
+			count++;
+			if (count == 1) {
+				//assert.equal(1,2);
+				assert.equal(html, "<html><body><h1>Hello world</h1><ul>");
+			} else if (count == 2) {
+				//console.log(html);
+				assert.equal(html, "<li></li></ul></body></html>");
+				done();
+			}
+			
+		});
+  	});
+
 });
 
 
