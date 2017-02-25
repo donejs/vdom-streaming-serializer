5c97f3970ea092732aec4cdcec4276ce2f133432
agc123 <agc123@yeah.net>
2017-02-25 00:56:27 -0600

    serialize elemtn

--- vdom-streaming-serializer.js
+++ vdom-streaming-serializer.js
@@ -1,3 +1,23 @@
-export default function() {
-  return `This is the vdom-streaming-serializer plugin`;
-}
+module.exports = function serialize(element){
+	//console.log(element);
+	if (element.nodeType === 3) {
+		return element.nodeValue;
+	}
+
+	var out = '<' + element.nodeName.toLowerCase();
+	for (var i = 0; i < element.attributes.length; i++) {
+		attr = element.attributes[i];
+		out += attr
+	}
+	out += '>';
+
+    var child = element.firstChild;
+    while(child) {
+    	out += serialize(child);
+    	child = child.nextSibling;
+    }
+	out += '</' + element.nodeName.toLowerCase() + '>'
+	return out;
+};
+
+
